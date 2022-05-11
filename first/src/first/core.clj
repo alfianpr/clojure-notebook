9e288ef35a10bf2a0e5554e8d5b41d6e66692ea6
alfianpr <alfianpratama@hotmail.com>
2022-05-12 02:48:51 +0700

    learn_1

--- first/src/first/core.clj
+++ first/src/first/core.clj
@@ -1,7 +1,36 @@
 (ns first.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(use 'first.core :reload)
+
+(println "hello, world")
+
+(def a "aaaaa")
+(println a)
+
+(let [l "light"] (println (str "hello " l)))
+
+(let [l "light"
+      l_d (str l " is made by")]
+  (println (str l_d " nature")))
+
+(def object "light")
+
+(println (str "let's be the " object))
+
+(defn say-hello "introduction"
+  [name]
+  (str "hi, my name is " name))
+
+(say-hello "alfian")
+
+(doc say-hello)
+
+(meta (var say-hello))
+
+(let [plus (fn [] (+ 1 %1 %2 %3))]
+  (println (plus 2 3 4)))
+
+(let [plus #(+ 1 %1 %2)]
+  (println (plus 2 3)))
+
